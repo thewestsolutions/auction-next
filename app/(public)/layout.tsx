8d1a98174dd1d3b865672da47daa9fcb0f637304
alexandruchitoraga <alexandru.chitoraga@gmail.com>
2025-03-07 11:32:40 -0800

    Install prettier

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,15 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="sticky top-0 z-10 bg-background">
-        <header className="w-full px-6 py-4 flex justify-between gap-8 border-b border-border">
+    <div className="flex min-h-screen flex-col">
+      <div className="bg-background sticky top-0 z-10">
+        <header className="border-border flex w-full justify-between gap-8 border-b px-6 py-4">
           <Image
             src="https://placehold.co/300x150.png"
             alt="logo"
@@ -29,22 +25,57 @@ export default function RootLayout({
           </div>
         </header>
 
-        <nav className="border-b border-border px-6">
+        <nav className="border-border border-b px-6">
           <div className="flex space-x-8">
-            <Link href="/" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">View All</Link>
-            <Link href="/staff-picks" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Staff Picks</Link>
-            <Link href="/top-deals" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Top Deals</Link>
-            <Link href="/last-chance" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Last Chance</Link>
-            <Link href="/popular" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Popular Now</Link>
-            <Link href="/recently-viewed" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Recently Viewed</Link>
-            <Link href="/for-you" className="py-4 text-sm font-medium text-muted-foreground hover:text-primary">Just for You</Link>
+            <Link
+              href="/"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              View All
+            </Link>
+            <Link
+              href="/staff-picks"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Staff Picks
+            </Link>
+            <Link
+              href="/top-deals"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Top Deals
+            </Link>
+            <Link
+              href="/last-chance"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Last Chance
+            </Link>
+            <Link
+              href="/popular"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Popular Now
+            </Link>
+            <Link
+              href="/recently-viewed"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Recently Viewed
+            </Link>
+            <Link
+              href="/for-you"
+              className="text-muted-foreground hover:text-primary py-4 text-sm font-medium"
+            >
+              Just for You
+            </Link>
           </div>
         </nav>
       </div>
 
       <main className="flex-grow px-6 py-4">{children}</main>
 
-      <footer className="mt-auto px-6 py-4 border-t border-border">
+      <footer className="border-border mt-auto border-t px-6 py-4">
         <div className="flex justify-between">
           <p className="text-muted-foreground">Footer</p>
           <p className="text-muted-foreground">Footer</p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
